ae01737a23cd711073d80c7c43289e107d87f03e
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-07-04 18:13:23 -0500

    Actions added

---
+++ lib/actions/add_item_action.dart
@@ -0,0 +1,7 @@
+import 'package:shopping_list/model/cart_item_model.dart';
+
+class AddItemAction {
+  final CartItem item;
+
+  AddItemAction(this.item);
+}
---
+++ lib/actions/toggle_state_action.dart
@@ -0,0 +1,7 @@
+import 'package:shopping_list/model/cart_item_model.dart';
+
+class ToggleItemStateAction {
+  final CartItem item;
+
+  ToggleItemStateAction(this.item);
+}
